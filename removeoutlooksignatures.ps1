32c6a8fe79cde36acbb44c871c50cf4d4c58abc2
adavidsonforgen <109097252+adavidsonforgen@users.noreply.github.com>
2022-10-16 10:03:18 -0600

    Create removeoutlooksignatures.ps1

---
+++ removeoutlooksignatures.ps1
@@ -0,0 +1,3 @@
+Set-ItemProperty -path "HKCU:\\Software\Microsoft\Office\16.0\Outlook\Profiles\Outlook\9375CFF0413111d3B88A00104B2A6676\00000002" -name "New Signature" -value ""
+
+Set-ItemProperty -path "HKCU:\\Software\Microsoft\Office\16.0\Outlook\Profiles\Outlook\9375CFF0413111d3B88A00104B2A6676\00000002" -name "Reply-Forward Signature" -value ""
